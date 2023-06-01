3eb7ae9897221e4f8527c5d817613f7fa68a5528
Lucas Smaniotto <lucas91755@gmail.com>
2023-06-01 11:58:38 -0300

    styles: criando estilos para o Pomodoro

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { PomodoroTimer } from './components/Pomodoro';
+import { Pomodoro } from './components/Pomodoro';
 
 function App() {
   return (
-    <div className="App">
-      <PomodoroTimer defaultPomodoroTime={1500} />
+    <div className="container">
+      <Pomodoro
+        pomodoroTime={1500}
+        shortRestTime={300}
+        longRestTime={900}
+        cycles={4}
+      />
     </div>
   );
 }
--- src/components/Pomodoro/index.tsx
+++ src/components/Pomodoro/index.tsx
@@ -4,21 +4,37 @@ import { Button } from '../Button';
 import { Timer } from '../Timer';
 
 interface Props {
-  defaultPomodoroTime: number;
+  pomodoroTime: number;
+  shortRestTime: number;
+  longRestTime: number;
+  cycles: number;
 }
 
-export function PomodoroTimer(props: Props): JSX.Element {
-  const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTime);
+export function Pomodoro(props: Props): JSX.Element {
+  const [mainTime, setMainTime] = React.useState(props.pomodoroTime);
 
   useInterval(() => {
     setMainTime(mainTime - 1);
   }, 1000);
 
   return (
-    <div className="pomodoro">
-      <h2>Status: Trabalhando</h2>
-      <Timer mainTime={mainTime} />
-      <Button text="Iniciar" onClick={() => console.log('Iniciou!')} />
-    </div>
+    <>
+      <div className="pomodoro">
+        <h2>Status:</h2>
+        <h3>Atividade</h3>
+        <Timer mainTime={mainTime} />
+        <div className="controls">
+          <Button text="Iniciar" onClick={() => console.log('Iniciou!')} />
+          <Button text="Iniciar" onClick={() => console.log('Iniciou!')} />
+          <Button text="Iniciar" onClick={() => console.log('Iniciou!')} />
+        </div>
+      </div>
+      <div className="details">
+        <p>Ciclos concluídos: 0</p>
+        <p>Horas trabalhadas: 00:00:00</p>
+        <p>Horas descansadas: 00:00:00</p>
+        <p>Próximo ciclo: Trabalho</p>
+      </div>
+    </>
   );
 }
